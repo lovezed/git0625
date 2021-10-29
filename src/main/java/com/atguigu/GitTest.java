28064ef7ce93eeb5317b4d13e0fc663017262f11
cxx <825010392@qq.com>
2021-10-29 20:32:35 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,7 +3,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
-        System.out.println("git test2");
-        System.out.println("git test");
+        System.out.println("git test1");
+        System.out.println("git tes2");
     }
 }
